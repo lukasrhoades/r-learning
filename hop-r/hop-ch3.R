73cd8cc4cc37f51481dc2ac761d78ec71c27a161
Lukas Rhoades <lukasrhoades11@gmail.com>
2025-04-13 11:52:24 -0700

    initial commit

---
+++ hop-r/hop-ch3.R
@@ -0,0 +1,43 @@
+# 3.1 Packages
+library("ggplot2")
+
+x <- c(-1, -0.8, -0.6, -0.4, -0.2, -0, 0.2, 0.4, 0.6, 0.8, 1)
+x
+
+y <- x^3
+y
+
+qplot(x, y)
+
+x <- c(1, 2, 2, 2, 3, 3)
+qplot(x, binwidth = 1)
+
+x2 <- c(1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4)
+qplot(x2, binwidth = 1)
+
+x3 <- c(0, 1, 1, 2, 2, 2, 3, 3, 4)
+qplot(x3, binwidth = 1)
+
+replicate(3, 1 + 1)
+
+replicate(10, roll())
+
+rolls <- replicate(10000, roll())
+qplot(rolls, binwidth = 1)
+
+# 3.2 Getting help from help pages
+?sqrt
+?log10
+?sample
+
+??log
+
+roll <- function() {
+  die <- 1:6
+  dice <- sample(die, size = 2, replace = TRUE,
+                 prob = c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8))
+  sum(dice)
+}
+
+rolls <- replicate(10000, roll())
+qplot(rolls, binwidth = 1)
